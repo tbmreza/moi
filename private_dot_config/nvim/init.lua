4d8f708b074fb9bbefb08c1109b6a1e8c0f7d312
Reza <rezahandzalah@gmail.com>
2026-09-26 21:59:44 +0700

    nvim telescope

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -4,6 +4,20 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- General options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wildignorecase = true
+
+-- Yank/copy to system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Cycle through tabs
+vim.keymap.set("n", "<Tab>", "gt", { desc = "Next tab" })
+vim.keymap.set("n", "<S-Tab>", "gT", { desc = "Previous tab" })
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -19,21 +33,4 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Configure Plugins
-require("lazy").setup({
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      
-      -- Keymaps
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
-    end,
-  },
-})
-
-require('gitsigns').setup()
+require("lazy").setup({ { import = "plugins" } })
